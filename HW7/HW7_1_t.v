722184b52384f2e1801221e8e0f0c641f13cd3d1
ChihMin <s102062111@m102.nthu.edu.tw>
2014-12-29 16:41:18 +0800

    add some test case to testbench

--- HW7/HW7_1_t.v
+++ HW7/HW7_1_t.v
@@ -18,12 +18,19 @@ module HW7_1_t;
 		clk = ~clk;
 	
 	initial fork
-		#10 rst_n = 1;
-		#10 in = 1;
-		#30 in = 0;
-		#50 in = 0;
-		#70 in = 1;
-		#90 in = 0;
+		#10		rst_n = 1;
+		#100	rst_n = 0;
+		#101	rst_n = 1;
+		#10		in = 1;
+		#30		in = 0;
+		#50		in = 0;
+		#70		in = 1;
+		#90		in = 0;
+		#110	in = 1;
+		#130	in = 1;
+		#150	in = 0;
+		#170	in = 1;
+		#190	in = 0;
 	join
 	
 	initial	begin
