d30aef3f7d1cdfeb548ea1c6b82f419b7609e532
ChihM <s102062111@m102.nthu.edu.tw>
2015-04-21 16:01:38 +0800

    complete lab07

--- lab07/keyboard_example.v
+++ lab07/keyboard_example.v
@@ -8,18 +8,22 @@ output [3:0] ENABLE;
 output [8:0] SEGMENT;
 
 reg [3:0] ROW;
+reg RESULT = 1'b0;
 reg [3:0] DEBOUNCE_COUNT;
 reg [3:0] DEBOUNCE_COUNT1;
 reg [3:0] DEBOUNCE_COUNT2;
 reg [3:0] SCAN_CODE;
-reg [3:0] DECODE_BCD;
+reg [4:0] DECODE_BCD;
+reg IS_BCD;
 reg [3:0] KEY_CODE;
 reg [3:0] ENABLE;
 reg [8:0] SEGMENT;
 reg [15:0] KEY_BUFFER;
-reg [15:0] OUTPUT_TEMP = 16'h1200;
+reg [15:0] OUTPUT_TEMP = 16'h0000;
 reg [14:1] DIVIDER;
-reg [3:0] A, B;
+reg [4:0] A, B;
+reg [4:0] ANS_A = 0;
+reg [4:0] ANS_B = 0;
 reg [4:0] ANS;
 reg FIRST = 1'b0;
 reg PRESS;
@@ -135,25 +139,49 @@ always@(negedge PRESS_VALID, negedge RESET)
 	end
 
 
-always@( negedge RESET, negedge PRESS_VALID, posedge ADD_VALID, posedge SUB_VALID)
+always@( negedge RESET, negedge PRESS_VALID, negedge ADD, negedge SUB)
 	begin
 		if(!RESET) begin
 			OUTPUT_TEMP = 16'd0;
 			KEY_BUFFER = 16'd0;
+			ANS_A = 5'd0;
+			ANS_B = 5'd0;
 		end
 	
-		else if(ADD_VALID == 1) begin
+		else if(ADD == 0 || SUB == 0) begin
 			A = (OUTPUT_TEMP & 16'hF000) >> 12;
 			B = (OUTPUT_TEMP & 16'h0F00) >> 8;
-			ANS = A + B;
+			A = (A & 5'b01000) == 5'b01000 ? (A | 5'b10000) : A;
+			B = (B & 5'b01000) == 5'b01000 ? (B | 5'b10000) : B;
 			
-			KEY_BUFFER = OUTPUT_TEMP | ANS;
-		end
-		else if(SUB_VALID == 1)begin
-			A = (OUTPUT_TEMP & 16'hF000) >> 12;
-			B = (OUTPUT_TEMP & 16'h0F00) >> 8;
-			ANS = A - B;
-			KEY_BUFFER = OUTPUT_TEMP | ANS;
+			if(!ADD) ANS = A + B;
+			else if(!SUB) ANS = A - B;
+			else	ANS = ANS;
+			
+			case (ANS) 
+				5'd0, 5'd1, 5'd2, 5'd3, 5'd4, 5'd5, 5'd6, 5'd7, 5'd8, 5'd9 : begin
+					ANS_A = 5'd0;
+					ANS_B = ANS;
+				end
+				5'd10, 5'd11, 5'd12, 5'd13, 5'd14, 5'd15: begin
+					ANS_A = 5'd1;
+					ANS_B = ANS - 5'd10;
+				end
+				5'd16, 5'd17, 5'd18, 5'd19, 5'd20, 5'd21, 5'd22: begin
+					ANS = ~ANS + 5'd1;
+					ANS_A = 5'd1;
+					ANS_B = ~(ANS - 5'd10)+5'd1;
+				end
+				5'd23, 5'd24, 5'd25, 5'd26, 5'd27, 5'd28, 5'd29, 5'd30, 5'd31: begin
+					ANS = ~ANS + 5'd1;
+					ANS_A = 5'd0;
+					ANS_B = ~(ANS)+5'd1;
+				end
+				default: begin
+					ANS_A = ANS_A;
+					ANS_B = ANS_B;
+				end
+			endcase
 		end
 	
 		else	begin
@@ -195,38 +223,76 @@ always @(negedge SCAN_CLK or negedge RESET)
 /****************************
 * Data Display Multiplexer *
 ****************************/
-always @(ENABLE or KEY_BUFFER)
+always @(ENABLE or KEY_BUFFER or ANS_A or ANS_B)
 	begin
 		case (ENABLE)
-			4'b1110:DECODE_BCD = KEY_BUFFER[3:0];
-			4'b1101:DECODE_BCD = KEY_BUFFER[7:4];
-			4'b1011:DECODE_BCD = KEY_BUFFER[11:8];
-			4'b0111:DECODE_BCD = KEY_BUFFER[15:12];
+			4'b1110: begin
+				DECODE_BCD = ANS_B;
+				IS_BCD = 1'b0;
+			end
+			4'b1101: begin
+				DECODE_BCD = ANS_A;
+				IS_BCD = 1'b0;
+			end
+			4'b1011: begin
+				DECODE_BCD = KEY_BUFFER[11:8];
+				IS_BCD = 1'b1;
+			end
+			4'b0111: begin
+				DECODE_BCD = KEY_BUFFER[15:12];
+				IS_BCD = 1'b1;
+			end
 		endcase
 	end
 
 /********************************
 * Hex To Seven Segment Decoder *
 ********************************/
-always @(DECODE_BCD)
+always @(DECODE_BCD, IS_BCD)
 	begin
-		case (DECODE_BCD)
-			4'h0 : SEGMENT = 9'b100000011;// 0
-			4'h1 : SEGMENT = 9'b110011111;//1
-			4'h2 : SEGMENT = 9'b100100100;//2
-			4'h3 : SEGMENT = 9'b100001100;//3
-			4'h4 : SEGMENT = 9'b110011000;//4
-			4'h5 : SEGMENT = 9'b101001000;//5
-			4'h6 : SEGMENT = 9'b101000000;//6
-			4'h7 : SEGMENT = 9'b100011111;//7
-			4'h8 : SEGMENT = 9'b000000000;//-8
-			4'h9 : SEGMENT = 9'b000011111;//-7
-			4'hA : SEGMENT = 9'b001000000;//-6
-			4'hB : SEGMENT = 9'b001001000;//-5
-			4'hC : SEGMENT = 9'b010011000;//-4
-			4'hD : SEGMENT = 9'b000001100;//-3
-			4'hE : SEGMENT = 9'b000100100;//-2
-			4'hF : SEGMENT = 9'b010011111;//-1
-		endcase
+		if(IS_BCD) begin
+			case (DECODE_BCD)
+				5'h0 : SEGMENT = 9'b100000011;// 0
+				5'h1 : SEGMENT = 9'b110011111;//1
+				5'h2 : SEGMENT = 9'b100100100;//2
+				5'h3 : SEGMENT = 9'b100001100;//3
+				5'h4 : SEGMENT = 9'b110011000;//4
+				5'h5 : SEGMENT = 9'b101001000;//5
+				5'h6 : SEGMENT = 9'b101000000;//6
+				5'h7 : SEGMENT = 9'b100011111;//7
+				5'h8 : SEGMENT = 9'b000000000;//-8
+				5'h9 : SEGMENT = 9'b000011111;//-7
+				5'hA : SEGMENT = 9'b001000000;//-6
+				5'hB : SEGMENT = 9'b001001000;//-5
+				5'hC : SEGMENT = 9'b010011000;//-4
+				5'hD : SEGMENT = 9'b000001100;//-3
+				5'hE : SEGMENT = 9'b000100100;//-2
+				5'hF : SEGMENT = 9'b010011111;//-1
+			endcase
+		end
+		else begin
+			case (DECODE_BCD)
+				5'h0 : SEGMENT = 9'b100000011;// 0
+				5'h1 : SEGMENT = 9'b110011111;//1
+				5'h2 : SEGMENT = 9'b100100100;//2
+				5'h3 : SEGMENT = 9'b100001100;//3
+				5'h4 : SEGMENT = 9'b110011000;//4
+				5'h5 : SEGMENT = 9'b101001000;//5
+				5'h6 : SEGMENT = 9'b101000000;//6
+				5'h7 : SEGMENT = 9'b100011111;//7
+				5'h8 : SEGMENT = 9'b100000000;//8
+				5'h9 : SEGMENT = 9'b100011000;//9
+				5'd23 : SEGMENT = 9'b000011000;//-9
+				5'd24 : SEGMENT = 9'b000000000;//-8
+				5'd25 : SEGMENT = 9'b000011111;//-7
+				5'd26 : SEGMENT = 9'b001000000;//-6
+				5'd27 : SEGMENT = 9'b001001000;//-5
+				5'd28 : SEGMENT = 9'b010011000;//-4
+				5'd29 : SEGMENT = 9'b000001100;//-3
+				5'd30 : SEGMENT = 9'b000100100;//-2
+				5'd31 : SEGMENT = 9'b010011111;//-1
+		
+			endcase
+		end
 	end
 endmodule 
